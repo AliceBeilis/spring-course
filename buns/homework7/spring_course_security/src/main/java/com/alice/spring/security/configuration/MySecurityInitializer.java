cd28249b7529ff061140c7100508e3d13d12df78
AliceBeilis <alicebeilis@mail.ru>
2022-12-05 18:24:22 +0500

    add project7

---
+++ buns/homework7/spring_course_security/src/main/java/com/alice/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.alice.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
